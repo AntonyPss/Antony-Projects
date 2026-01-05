bea580b475629a743b36251872c6a0842218fda0
Antony Pss <116685752+AntonyPss@users.noreply.github.com>
2026-01-04 19:41:22 -0500

    New Design...

--- js/main.js
+++ js/main.js
@@ -31,29 +31,24 @@ window.addEventListener("scroll", function () {
     }
 });
 
-// Show Neko Image Hover
 document.addEventListener("DOMContentLoaded", () => {
+    // Neko Image Cursor
     const trigger = document.querySelector(".neko-trigger");
     const nekoImg = document.getElementById("neko-img");
 
-    // 1. Mostrar el Neko cuando entra el mouse
     trigger.addEventListener("mouseenter", () => {
         nekoImg.classList.add("is-visible");
     });
 
-    // 2. Mover el Neko con el mouse
     trigger.addEventListener("mousemove", (e) => {
-        // Ajustamos la posición (e.clientX es la X del mouse)
-        // Le restamos la mitad del ancho para centrarlo y restamos en Y para que esté "encima"
         const x = e.clientX;
         const y = e.clientY;
 
-        // Ajusta los números (-40 y -90) según el tamaño de tu imagen
         nekoImg.style.left = `${x - 40}px`;
         nekoImg.style.top = `${y - 90}px`;
     });
 
-    // 3. Ocultar el Neko cuando sale el mouse
+    //
     trigger.addEventListener("mouseleave", () => {
         nekoImg.classList.remove("is-visible");
     });
